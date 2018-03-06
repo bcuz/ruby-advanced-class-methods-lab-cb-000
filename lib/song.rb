0446d9051bf846a6c8e1e6fe1fd94ff05a8d62be
adam n <adam.nawroc@gmail.com>
2018-03-06 11:53:59 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,49 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    @@all << song
+    song
+  end
+
+  def self.new_by_name(name)
+    # song.name = 'countdown'
+    # song.artist_name = 'journey'
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    @@all << song
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.detect{|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    x = self.find_by_name(name)
+
+    if x.nil?
+      self.create_by_name(name)
+    else
+      x
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |k| k.name }
+  end
+
+  def self.new_from_filename()
+    song = self.new
+    song.name = str[0]
+    song.artist_name = str[1]
+    song
+  end
 end
